85a033125d49c1141ae719058a2ef4d118cb7c06
julianParrax <julianfranco.parra@gmail.com>
2022-11-25 02:49:14 -0300

    Se agrega app con DateTime

---
+++ 3-WinForm/4-Tipo-DateTime/Manejo-de-Fechas-con-DateTime/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace Manejo_de_Fechas_con_DateTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.calFecha = new System.Windows.Forms.MonthCalendar();
+            this.btnPrueba1 = new System.Windows.Forms.Button();
+            this.btnPrueba2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Location = new System.Drawing.Point(132, 103);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(200, 20);
+            this.dtpFecha.TabIndex = 0;
+            // 
+            // calFecha
+            // 
+            this.calFecha.Location = new System.Drawing.Point(84, 148);
+            this.calFecha.Name = "calFecha";
+            this.calFecha.TabIndex = 1;
+            // 
+            // btnPrueba1
+            // 
+            this.btnPrueba1.Location = new System.Drawing.Point(359, 100);
+            this.btnPrueba1.Name = "btnPrueba1";
+            this.btnPrueba1.Size = new System.Drawing.Size(75, 23);
+            this.btnPrueba1.TabIndex = 2;
+            this.btnPrueba1.Text = "Prueba 1";
+            this.btnPrueba1.UseVisualStyleBackColor = true;
+            this.btnPrueba1.Click += new System.EventHandler(this.btnPrueba1_Click);
+            // 
+            // btnPrueba2
+            // 
+            this.btnPrueba2.Location = new System.Drawing.Point(359, 148);
+            this.btnPrueba2.Name = "btnPrueba2";
+            this.btnPrueba2.Size = new System.Drawing.Size(75, 23);
+            this.btnPrueba2.TabIndex = 3;
+            this.btnPrueba2.Text = "Prueba 2";
+            this.btnPrueba2.UseVisualStyleBackColor = true;
+            this.btnPrueba2.Click += new System.EventHandler(this.btnPrueba2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 407);
+            this.Controls.Add(this.btnPrueba2);
+            this.Controls.Add(this.btnPrueba1);
+            this.Controls.Add(this.calFecha);
+            this.Controls.Add(this.dtpFecha);
+            this.MaximumSize = new System.Drawing.Size(681, 446);
+            this.MinimumSize = new System.Drawing.Size(498, 399);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.MonthCalendar calFecha;
+        private System.Windows.Forms.Button btnPrueba1;
+        private System.Windows.Forms.Button btnPrueba2;
+    }
+}
+
---
+++ 3-WinForm/4-Tipo-DateTime/Manejo-de-Fechas-con-DateTime/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manejo_de_Fechas_con_DateTime
+{
+    public partial class Form1 : Form
+    {
+        //dateTime() es un tipo de dato; es un struct y tiene muchas ampliar utilidades en la documentacion. el Struct DateTime tiene constructores con muchas sobrecargas
+        //para ver las tipos de propiedades y las sobrecargas de constructor que se pueden utilizar de dateTime(), inicializar el componente y acceder a la definicion
+
+        //En este caso vamos a capturar la fecha seleccionada en el calendario y la vamos a mostrar por pantalla.
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrueba1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("La fecha seleccionada es: " + dtpFecha.Value.ToString("dd/MM/yyyy")); //value devuelve un tipo de dato struct de Date time
+            //Igualemente, el DateTimePicker se utilizar para capturar la fecha y guardarla, como en el siguiente ejemplo.
+            DateTime fecha1;
+            fecha1 = dtpFecha.Value; //cuando queremos capturar un valor no utilizamos to string ni otros metodos,sino que lo guardamos en formato PURO.los formatos los utilizamosd despues
+            //caputuramos la fecha, la guardamos en una variable y trabajamos con esa variable.
+            //el mismo ejemplo comentado arriba pero con los datos capturados
+            MessageBox.Show("La fecha seleccionada es: " + fecha1.ToString("dd/MM/yyyy"));
+        }
+
+        private void btnPrueba2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("La fecha seleccionada es: " + calFecha.SelectionStart.ToString("dd/MM/yyyy"));//este tiene mas funcionalidades y metodos que el dateTimePicker
+        }
+    }
+}
+
+
+//DATO: cuando manipulamos fechas y bases de datos, tenemos problemas de formato al usar los datos de las fechas en formato string,
+//por lo que es recomendable es capturar y guardar el dato puro de la fecha en una variable, PURO. MANDAMOS EL VALOR PURO A UNA BASE DE 
+//DATOS; osea, guardamos el dato en una variable y lo mandamos a la base de datos. Desoues cuando los tengamos que utilizar, si podemos
+//jugar con el formato que le vamos a dar.
