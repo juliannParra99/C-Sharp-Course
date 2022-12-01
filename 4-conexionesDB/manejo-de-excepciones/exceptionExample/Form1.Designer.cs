d0522951f4b9e4d522ffadc6e2610f0ae3ada72f
julianParrax <julianfranco.parra@gmail.com>
2022-12-01 01:34:52 -0300

    Se agruega Try catch finally

---
+++ 4-conexionesDB/manejo-de-excepciones/exceptionExample/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace exceptionExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.tx2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(197, 217);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(16, 13);
+            this.lblResultado.TabIndex = 0;
+            this.lblResultado.Text = "= ";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(185, 173);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "button1";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(166, 70);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(117, 20);
+            this.txt1.TabIndex = 2;
+            // 
+            // tx2
+            // 
+            this.tx2.Location = new System.Drawing.Point(166, 138);
+            this.tx2.Name = "tx2";
+            this.tx2.Size = new System.Drawing.Size(117, 20);
+            this.tx2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 313);
+            this.Controls.Add(this.tx2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblResultado);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox tx2;
+    }
+}
+
---
+++ 4-conexionesDB/manejo-de-excepciones/exceptionExample/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//2 tipos de errores : 
+//error de compilacion: error de sintaxisd, se escribio mal algo; el compilador no puede compilar, es sensillo de resolver por que el compilador
+//dice donde estan las lineas.
+// Error de excepcion: error no esperado en algun momento de la ejecucion; sucede en un momento excepcional.
+// el programa compila pero en un punto el programa se detiene, falla abruptamente en algun momento etc.error ent tiempo de ejecucion
+//las excepciones permiten que preparemos nuestras app para que en escenarios no pensados la aplicacion no crashee.
+//el manejo de excepciones nos permiten capturar el error en el  momento, y en vez de que la app responda de manera defectuosa, nuestra
+//app este preparada para esa circunstancia, y hace algo en consecuencia: validar el dato, mandar un mail etc. 
+//para eso usamos un try catch
+namespace exceptionExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //para manejar las excepciones: si coloco una letra en lugar de un numero, me devuelve una excepcion por que no es el formato
+            //esperado, por lo que lo capturamos.
+            //try va a intentar ejecutar el codigo de dentro
+            //las intrucciones operatorias con logicas van dentro
+
+            //el catch lo que  hace es ir recorriendo linea por linea y verifica si se ejecuto y pasa a la siguiente, si hay un error,
+            //el bloque catch lo captura como excepcion y trabaja y trata que se va a realizar cuando ocurre la excepcion
+            //puedo poner tantos catch como yo quiera de acuerdo a las excepciones que quiera manejar:exeption  es la general
+            int resultado;
+            try
+            {
+                resultado = Calcular();
+                lblResultado.Text = " = " + resultado;
+            }
+            catch (Exception ex)//exception general.
+            {
+                MessageBox.Show("Error no reconocido, contacte a su dev: " + ex.ToString()); 
+            }
+            finally
+            {
+        //el finally se va a ejecutar si o si, no importa si hay un error en el try y salta al catch con codigo que no termino de
+        //ejecturarse; despues del catch se va a ejecutar. ej. se abre base de datos en try, hay error salta al catch, pero no se cierra
+        //la DB por lo que en el finally se ejecuta que ahi se cierra la DB
+
+            }
+        }   
+        
+        private int Calcular() // este metodo se pasa al evento click, genera un doble try catch OJO CON ESTO
+        {
+                int a, b, r;//las inicializaciones pueden estar fuera
+            try
+            {
+                a = int.Parse(txt1.Text);
+                b = int.Parse(tx2.Text);//aca hay un error por que no se pueden transformar una letra en entero; lanza exeption y va al catch
+                r = a + b;
+                return r;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex; //lanza una excepcion si algo sale mal; LOS METODOS QUE PUEDEN DEVOLVER UNA EXCEPCION TAMBIEND DEBEN ESTAR CAPTURADOS EN UN TRY CATCH
+            }
+        }
+
+    }
+}
+
