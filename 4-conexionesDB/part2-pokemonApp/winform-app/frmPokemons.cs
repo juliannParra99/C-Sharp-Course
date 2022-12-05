c9f3c1eedacf45817efa743258a0e01ad15ef0dc
julianParrax <julianfranco.parra@gmail.com>
2022-12-05 10:19:58 -0300

    Se agrega la parte dos de la pokeApp

---
+++ 4-conexionesDB/part2-pokemonApp/winform-app/frmPokemons.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform_app
+{
+    public partial class frmPokemons : Form
+    {
+        private List<Pokemon> listaPokemon;
+        public frmPokemons()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPokemons_Load(object sender, EventArgs e)
+        {
+            PokemonNegocio negocio = new PokemonNegocio();
+            listaPokemon = negocio.listar();
+            dgvPokemons.DataSource = listaPokemon;
+            cargarImagen(listaPokemon[0].UrlImagen);
+        }
+
+        private void dgvPokemons_SelectionChanged(object sender, EventArgs e) //control agregado para que pueda mostrar la imagen
+        {
+            Pokemon seleccionado = (Pokemon)dgvPokemons.CurrentRow.DataBoundItem;
+            cargarImagen(seleccionado.UrlImagen);
+        }
+
+        //manejo de errores: en caso de que acceda a la imagen y en caso que rechace trae una imagen de internet
+        private void cargarImagen(string imagen)
+        {
+            try
+            {
+                pbxPokemon.Load(imagen);
+            }
+            catch (Exception ex)
+            {
+                pbxPokemon.Load("https://efectocolibri.com/wp-content/uploads/2021/01/placeholder.png");
+            }
+        }
+    }
+}
